455de77611f8e9324fecd27702437775e627ee69
claudiavgaleano <aldebaranpf@gmail.com>
2018-09-06 00:39:13 -0300

    Ejercicio de dar vuelta un array

---
+++ EloquentJavascript/Chapter4/reversinAnArray.js
@@ -0,0 +1,21 @@
+const reverseArray = (arrayValue) => {
+  let result = [];
+  for (let n = arrayValue.length - 1; n >= 0; n--) {    
+    result.push(arrayValue[n]);
+  }
+  return result;
+}
+
+console.log(reverseArray (["B","A","D","O","O"]));
+
+const reverseArrayInPlace = (array) => {
+	for (let n = 0; n < array.length - n; n++) {
+		let aux = array[n];
+		array[n] = array[array.length - (n+1)];
+		array[array.length - (n+1)] = aux;
+	}
+	return array;
+}
+
+
+console.log(reverseArray (["E","L","O","B","I"]));
